501d49c100d6abcd0f097920b26cf4b038f4e2f0
pratajski <p.ratajski@gmail.com>
2018-08-13 21:17:32 +0200

    poszukiwanie bledu

---
+++ src/main/java/com/crud/tasks/mapper/TaskMapper.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.mapper;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+
+public class TaskMapper {
+    public Task mapToTask (final TaskDto taskDto) {
+        return new Task (
+                taskDto.getId(),
+                taskDto.getTitle(),
+                taskDto.getContent());
+    }
+}
