6a42b1d154d3f98f15752d2be61c60f5c24537fa
pratajski <p.ratajski@gmail.com>
2018-08-25 09:48:53 +0200

    18,3 cd

--- src/main/java/com/crud/tasks/trello/client/AttachmentsByType.java
+++ src/main/java/com/crud/tasks/trello/client/AttachmentsByType.java
@@ -4,21 +4,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CreatedTrelloCard {
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("name")
-    private String name;
-
-    @JsonProperty("shortUrl")
-    private String shortUrl;
-
-    @JsonProperty("badges")
-    private Badges badges;
-
+public class AttachmentsByType {
+    @JsonProperty("trello")
+    private Trello trello;
 }
